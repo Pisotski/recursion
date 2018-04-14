e8675ae44d1138662cd25115532bfbe703023925
Poisk <vpisotski@gmail.com>
2018-04-13 20:01:59 -0700

    completed JSON stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,30 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+
+  if (Array.isArray(element)) {
+    result += '[';
+    result += element.map(function(el) {
+      return stringifyJSON(el);
+    }).join(',');
+    result += ']';
+  } else if (typeof element === 'object' && element !== null) {
+    result += '{';
+    for (var key in element) {
+      if (element[key] !== undefined && typeof element[key] !== 'function') {
+        result += '"' + key + '"' + ':' + stringifyJSON( element[key] ) + ',';
+      }
+    }
+    if (result.length > 1) {
+      result = result.slice(0, result.length - 1) + '}'; 
+    } else {
+      result += '}';
+    }
+  } else if (typeof element === 'string') {
+    result += '"' + element + '"';
+  } else {
+    result += element;
+  }
+  return result;
 };
